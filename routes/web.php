c0c664ae71b75a4c0b8da454e684b024bbb1483c
FUJITANImasataka <startallover2008@yahoo.co.jp>
2022-12-04 14:36:53 +0000

    phplaravel09task

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\controller\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+//http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定
+
+Route::controller(aaaController::class)->group(function() {
+    Route:get('xxx', 'bbb');
+});
