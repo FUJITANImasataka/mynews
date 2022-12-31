d45a3cb5f4cf7e24055f62b0f95bcea52bcbb7a7
FUJITANImasataka <startallover2008@yahoo.co.jp>
2022-12-31 14:02:14 +0000

    phplaravel15homework

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,8 +14,27 @@ class ProfileController extends Controller
            return view('admin.profile.create');
     }
     
-     public function create()
+     public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+        
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+        
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
